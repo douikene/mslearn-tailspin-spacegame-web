8c0d686970f78ce84b6a4395bc207a1c4550aa08
Djamel Ouikene <d.ouikene@arkadin.com>
2020-07-23 10:51:58 +0800

    commit playground change 2

--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -24,6 +24,7 @@ namespace TailSpin.SpaceGame.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //put a coment here 2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
